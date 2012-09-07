b1c2a7b5df11e5ec915c0c251d27fb48edfdbec6
U-IPLK-LAPTOP\iplk <iplk@iplk-laptop.(none)>
2012-09-08 10:14:32 +1200

    Added Two Behaviours, one for sky and one for tiles

--- Epheremal/Epheremal/Epheremal/Model/Entity.cs
+++ Epheremal/Epheremal/Epheremal/Model/Entity.cs
@@ -39,11 +39,13 @@ namespace Epheremal.Model
          */
         public void AssignBehaviour(Dictionary<EntityState, List<Behaviour>> behaviours) {
             this.Behaviours = behaviours;
+            /*
             Behaviours = new Dictionary<EntityState, List<Behaviour>>()
             {
                 {EntityState.GOOD, new List<Behaviour>()},
                 {EntityState.BAD, new List<Behaviour>()},
             };
+             */
         }
 
         public abstract Interaction[] GetInteractionsFor(Character interactor);
