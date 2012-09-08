9187ecd05447a5aeda8abfc59c3fdadfd46e6a3a
kjgorman <kieran.j.gorman@gmail.com>
2012-09-08 14:50:09 +1200

    ...and mirror changes into bounce

--- Epheremal/Epheremal/Epheremal/Model/Interactions/Bounce.cs
+++ Epheremal/Epheremal/Epheremal/Model/Interactions/Bounce.cs
@@ -13,6 +13,12 @@ namespace Epheremal.Model.Interactions
 
         public override void Interact()
         {
+            applyTo(Interactor, Interactee);
+            if(Interactee is Character) applyTo((Character)Interactee, Interactor);
+        }
+
+        public void applyTo(Character Interactor, Entity Interactee)
+        {
             double dx = Interactor.GetX() - Interactee.GetX();
             double dy = Interactor.GetY() - Interactee.GetY();
             double yVel = ((Character)Interactor).YVel;
