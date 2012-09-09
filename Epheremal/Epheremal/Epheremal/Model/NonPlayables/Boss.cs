b6424e5bc62b0872efd79fc31694e339258d25ee
eiskalt <eiskaltnz@gmail.com>
2012-09-09 14:59:50 +1200

    Working on boss

--- Epheremal/Epheremal/Epheremal/Model/NonPlayables/Boss.cs
+++ Epheremal/Epheremal/Epheremal/Model/NonPlayables/Boss.cs
@@ -9,31 +9,43 @@ using Microsoft.Xna.Framework;
 
 namespace Epheremal.Model.NonPlayables
 {
+
+
     class Boss : NPC
     {
-        public Boss(TileMap tileMap, int tileIDGood, int tileIDBad)
+        int IDGood2, IDGood3, IDGood4, IDBad2, IDBad3, IDBad4;
+        int[] goodTiles, badTiles;
+
+        public Boss(TileMap tileMap, int tileIDGood, int tileIDGood2, int tileIDGood3, int tileIDGood4, int tileIDBad, int tileIDBad2, int tileIDBad3, int tileIDBad4)
             : base(tileMap, tileIDGood, tileIDBad)
         {
-
+            IDGood2 = tileIDGood2;
+            IDGood3 = tileIDGood3;
+            IDGood4 = tileIDGood4;
+            IDBad2 = tileIDBad2;
+            IDBad3 = tileIDBad3;
+            IDBad4 = tileIDBad4;
             this.Behaviours = new Dictionary<EntityState, List<Behaviour>>();
 
             this.Behaviours.Add(EntityState.GOOD,
                                 new List<Behaviour>()
                                 {
-                                    new Deadly(),
-                                    new Flies(true),
-                                    new MovePatrolVert(30,1)
+                                    new MovePatrol(3,3,1),
+                                    new Deadly()   
                                 });
-
-
             this.Behaviours.Add(EntityState.BAD,
                                 new List<Behaviour>()
                                 {
-                                    new Inanimate(),
-                                    new Harmless()
+                                    new Adhesive(),
+                                    new MovePatrol(3,3,1)
                                 });
-            _width *= 3;
-            _height *= 3;
+            _width *= 4;
+            _height *= 4;
+
+            int[] gt = { _tileIDGood, IDGood2, IDGood3, IDGood4 };
+            int[] bt = { _tileIDBad, IDBad2, IDBad3, IDBad4 };
+            goodTiles = gt;
+            badTiles = bt;
         }
 
         public override Rectangle GetBoundingRectangle()
@@ -42,23 +54,44 @@ namespace Epheremal.Model.NonPlayables
             return new Rectangle(xPosition, yPosition, this._width, this._height);
         }
 
+        public Rectangle GetSmallBoundingRectangle(int x, int y)
+        {
+            int xPosition = Convert.ToInt32(PosX - Engine.xOffset), yPosition = Convert.ToInt32(PosY - Engine.yOffset);
+            return new Rectangle(xPosition + x * Block.BLOCK_WIDTH, yPosition + y * Block.BLOCK_WIDTH, Block.BLOCK_WIDTH, Block.BLOCK_WIDTH);
+        }
+
         public override SpriteBatch RenderSelf(ref SpriteBatch sprites)
         {
             Color tint = Color.White;
 
-            if (Entity.State == EntityState.GOOD)
-                if (XVel < 0)
-                    sprites.Draw(this._tileMap.TileMapTexture, this.GetBoundingRectangle(), _tileMap.getRectForTile(_tileIDGood), tint);
-                else
-                    sprites.Draw(this._tileMap.TileMapTexture, this.GetBoundingRectangle(), _tileMap.getRectForTile(_tileIDGood + 1), tint);
+            int[] tile = goodTiles;
+            if (Entity.State == EntityState.BAD) tile = badTiles;
 
-            else
-                if (XVel < 0)
-                    sprites.Draw(this._tileMap.TileMapTexture, this.GetBoundingRectangle(), _tileMap.getRectForTile(_tileIDBad), tint);
-                else
-                    sprites.Draw(this._tileMap.TileMapTexture, this.GetBoundingRectangle(), _tileMap.getRectForTile(_tileIDBad + 1), tint);
-            
-            
+            int modifier = 1;
+            if (XVel < 0) modifier = 3;
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    sprites.Draw(this._tileMap.TileMapTexture, this.GetSmallBoundingRectangle(j, i), _tileMap.getRectForTile(tile[i] + (j)), tint);
+
+                }
+            }
+
+            /*
+
+            int[] tile = { _tileIDGood, IDGood2, IDGood3, IDGood4 };
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    sprites.Draw(this._tileMap.TileMapTexture, this.GetSmallBoundingRectangle(j, i), _tileMap.getRectForTile(tile[i] + j), tint);
+
+                }
+            }*/
             return sprites;
         }
 
